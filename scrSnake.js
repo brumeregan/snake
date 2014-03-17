b66e8048666589561205307e0423a106c5d9b96d
Korvin <korvin1986@gmail.com>
2014-03-17 23:36:36 +0200

    update jQuery library refactor code

--- scrSnake.js
+++ scrSnake.js
@@ -4,29 +4,61 @@
 $(document).ready(function(){
 
     var field = document.getElementById('gameDiv');
-    var snake = document.createElement('div');
+    var head = document.createElement('div');
+    head.id = "head";
+    field.appendChild(head);
+    var hh = $('#head');
+    hh.toggleClass('snakeblock');
+    hh.css({
+        backgroundColor: 'red',
+        left: Math.floor(Math.random()*300) + 'px',
+        top: Math.floor(Math.random()*300) + 'px'
+    });
 
-    snake.style.width = '10px';
+
+    /*snake.style.width = '10px';
     snake.style.height = '10px';
     snake.style.border = '1px solid white';
     snake.style.backgroundColor="black";
+
     snake.style.margin = Math.random()*300 + 'px';
+*/
 
-    field.appendChild(snake);
+    var tail = document.createElement('div');
+    tail.id = 'tail';
+    field.appendChild(tail);
+    var tt = $('#tail');
+    tt.toggleClass('snakeblock');
+    tt.css({
+        'background-color': 'blue',
+        top: hh.css('top'),
+        left: parseInt(hh.css('left')) + 15 + 'px'
+    });
 
-    var snake1 = document.createElement('div');
-    snake1.style.width = '10px';
+    /*snake1.style.width = '10px';
     snake1.style.height = '10px';
     snake1.style.border = '1px solid white';
     snake1.style.backgroundColor ="black";
     snake1.style.margin =
         snake.style.margin + 10 + 'px';
+*/
+    $('.snakeblock').css({
+        width: '10px',
+        height: '10px',
+        border: 'solid white 1px',
+        position: 'absolute',
+        margin: '5px'
+    });
+
+
+
+
    /* fucntion (){
         var rand = Math.random();
         if(rand < 0.25){
             snake1.style.margin =
         }}*/
-    field.appendChild(snake1);
+
 
   /*  function pressK(key){
         switch(key){
@@ -49,4 +81,4 @@ $(document).ready(function(){
     };*/
 
 
-})
+});
