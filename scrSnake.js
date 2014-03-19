492af5bfc26c52c1adfe11bd8479ddad3414c244
kerkins <brumeregan@gmail.com>
2014-03-19 21:30:13 +0200

    Initial commit

--- scrSnake.js
+++ scrSnake.js
@@ -11,18 +11,9 @@ $(document).ready(function(){
     hh.toggleClass('snakeblock');
     hh.css({
         backgroundColor: 'red',
-        left: Math.floor(Math.random()*285) + 'px',
-        top: Math.floor(Math.random()*285) + 'px'
+        left: Math.floor(Math.random()*280) + 'px',
+        top: Math.floor(Math.random()*280) + 'px'
     });
-
-
-    /*snake.style.width = '10px';
-    snake.style.height = '10px';
-    snake.style.border = '1px solid white';
-    snake.style.backgroundColor="black";
-
-    snake.style.margin = Math.random()*300 + 'px';
-*/
 
     var tail = document.createElement('div');
     tail.id = 'tail';
@@ -31,39 +22,32 @@ $(document).ready(function(){
     tt.toggleClass('snakeblock');
     tt.css({
         'background-color': 'blue'
-       // top: hh.css('top'),
-      //  left: parseInt(hh.css('left')) + 15 + 'px'
+
     });
     var rand = Math.random() * 10;
     if(rand<=2.5){
         tt.css({
             top: hh.css('top'),
-            left: parseInt(hh.css('left')) - 15 +'px'
+            left: parseInt(hh.css('left')) - 12 +'px'
         })
     } else if(rand > 2.5 && rand <=5){
         tt.css({
             top: hh.css('top'),
-            left: parseInt(hh.css('left')) + 15 +'px'
+            left: parseInt(hh.css('left')) + 12 +'px'
         })
     } else if(rand > 5 && rand <= 7.5){
         tt.css({
-            top: parseInt(hh.css('top')) + 15 + 'px',
+            top: parseInt(hh.css('top')) - 12 + 'px',
             left: hh.css('left')
         })
     } else{
         tt.css({
-            top: parseInt(hh.css('top')) + 25 + 'px',
+            top: parseInt(hh.css('top')) + 12+ 'px',
             left: hh.css('left')
         })
     }
 console.log(rand);
-    /*snake1.style.width = '10px';
-    snake1.style.height = '10px';
-    snake1.style.border = '1px solid white';
-    snake1.style.backgroundColor ="black";
-    snake1.style.margin =
-        snake.style.margin + 10 + 'px';
-*/
+
     $('.snakeblock').css({
         width: '10px',
         height: '10px',
@@ -73,13 +57,15 @@ console.log(rand);
     });
 
 
+for(var i = hh.css('top'); i<300; i+=10){
+    $('#head').animate(
+    hh.css({
+        top: parseInt(hh.css('top')) + 12 + 'px',
+        left: hh.css('left')
+    })
+    )
+}
 
-
-   /* fucntion (){
-        var rand = Math.random();
-        if(rand < 0.25){
-            snake1.style.margin =
-        }}*/
 
 
   /*  function pressK(key){
